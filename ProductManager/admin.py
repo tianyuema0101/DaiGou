129a9a72b0568059c0c46150cf41e053a12ca2a0
tianyuema0101 <30672481+tianyuema0101@users.noreply.github.com>
2018-12-16 14:35:24 +1100

    V0.1.1 Initial database

--- ProductManager/admin.py
+++ ProductManager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import *
 
 # Register your models here.
+# Re-register UserAdmin
+#admin.site.register(User)
+#admin.site.register(User, UserAdmin)
+
+admin.site.register(Product)
+admin.site.register(Image)
--- ProductManager/models.py
+++ ProductManager/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
+class Product(models.Model):
     auto_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, default="Not Created")
+    Type = models.CharField(max_length=50, null=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    create_date = models.DateTimeField(auto_now_add=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='img', null=True)
